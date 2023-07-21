f7d4f428afcbadfcf08c4ffcf5c15237d78da602
anuj raghuwanshi <anujonline46@gmail.com>
2023-07-21 14:12:26 +0530

    chat-app

---
+++ server.js
@@ -0,0 +1,21 @@
+
+// import express
+const express = require('express')
+
+// make an express app
+const app = express();
+
+// making server using http and express
+const server = require('http').Server(app);
+ 
+//it will pick index.html
+app.use(express.static('public'));
+
+const PORT = 9000;
+
+
+    
+server.listen(PORT , () => {
+    console.log(`server is running on PORT ${PORT}`)
+})
+ 
